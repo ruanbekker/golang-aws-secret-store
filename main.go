9cb98f98c2450919c409bdd9208e81dc266cba3b
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-04-30 09:03:37 +0200

    add encrypt method

--- main.go
+++ main.go
@@ -1,6 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
+)
+
+func encryptSecret(secretValue string) (string, error) {
+
+	var kmsRegion string
+	var kmsKey string
+
+	kmsRegion = os.Getenv("KMS_REGION")
+	kmsKey = os.Getenv("KMS_KEY")
+
+	s := session.Must(session.NewSession())
+	svc := kms.New(s, &aws.Config{
+		Region: aws.String(kmsRegion),
+	})
+
+	params := &kms.EncryptInput{
+		KeyId:     aws.String(kmsKey),
+		Plaintext: []byte(secretValue),
+	}
+
+	resp, err := svc.Encrypt(params)
+	if err != nil {
+		return "", err
+	}
+
+	cipher := string(resp.CiphertextBlob[:])
+	return cipher, nil
+
+}
 
 func main() {
 	fmt.Println("Hello, World")
