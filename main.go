bbdbdc779788a0475da6956a061bb5dba6a60a5f
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-04-30 09:04:57 +0200

    add decrypt method

--- main.go
+++ main.go
@@ -37,6 +37,33 @@ func encryptSecret(secretValue string) (string, error) {
 
 }
 
+func decryptSecret(encryptedCipher string) (string, error) {
+
+	var kmsRegion string
+	kmsRegion = os.Getenv("KMS_REGION")
+
+	s := session.Must(session.NewSession())
+	svc := kms.New(s, &aws.Config{
+		Region: aws.String(kmsRegion),
+	})
+
+	blob := []byte(encryptedCipher)
+
+	params := &kms.DecryptInput{
+		CiphertextBlob: blob,
+	}
+
+	resp, err := svc.Decrypt(params)
+
+	if err != nil {
+		fmt.Println("Error decrypting:", err)
+		os.Exit(1)
+	}
+
+	blobString := string(resp.Plaintext)
+	return blobString, nil
+}
+
 func main() {
 	fmt.Println("Hello, World")
 }
